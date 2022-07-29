64428e7a08d9c1636d6dcb36854af5ad2da47ae8
lewisxhe <lewishe@outlook.com>
2022-07-29 10:50:22 +0800

    Added SX1280 Example

---
+++ examples/RadioLibExamples/SX1280/SX128x_Transmit_Interrupt/SX128x_Transmit_Interrupt.ino
@@ -0,0 +1,157 @@
+/*
+   RadioLib SX128x Transmit with Interrupts Example
+
+   This example transmits LoRa packets with one second delays
+   between them. Each packet contains up to 256 bytes
+   of data, in the form of:
+    - Arduino String
+    - null-terminated char array (C-string)
+    - arbitrary binary data (byte array)
+
+   Other modules from SX128x family can also be used.
+
+   For default module settings, see the wiki page
+   https://github.com/jgromes/RadioLib/wiki/Default-configuration#sx128x---lora-modem
+
+   For full API reference, see the GitHub Pages
+   https://jgromes.github.io/RadioLib/
+*/
+
+
+#include <RadioLib.h>
+#include "boards.h"
+
+SX1280 radio = new Module(RADIO_CS_PIN, RADIO_DI0_PIN, RADIO_RST_PIN, RADIO_BUSY_PIN);
+
+// save transmission state between loops
+int transmissionState = ERR_NONE;
+
+// flag to indicate that a packet was sent
+volatile bool transmittedFlag = false;
+
+// disable interrupt when it's not needed
+volatile bool enableInterrupt = true;
+
+uint32_t counter = 0;
+
+// this function is called when a complete packet
+// is transmitted by the module
+// IMPORTANT: this function MUST be 'void' type
+//            and MUST NOT have any arguments!
+void setFlag(void)
+{
+    // check if the interrupt is enabled
+    if (!enableInterrupt) {
+        return;
+    }
+
+    // we sent a packet, set the flag
+    transmittedFlag = true;
+}
+
+void setup()
+{
+    initBoard();
+    // When the power is turned on, a delay is required.
+    delay(1500);
+
+    // initialize SX1280 with default settings
+    Serial.print(F("[SX1280] Initializing ... "));
+    int state = radio.begin();
+#ifdef HAS_DISPLAY
+    if (u8g2) {
+        if (state != ERR_NONE) {
+            u8g2->clearBuffer();
+            u8g2->drawStr(0, 12, "Initializing: FAIL!");
+            u8g2->sendBuffer();
+        }
+    }
+#endif
+    if (state == ERR_NONE) {
+        Serial.println(F("success!"));
+    } else {
+        Serial.print(F("failed, code "));
+        Serial.println(state);
+        while (true);
+    }
+
+    // set the function that will be called
+    // when packet transmission is finished
+    radio.setDio1Action(setFlag);
+
+    // start transmitting the first packet
+    Serial.print(F("[SX1280] Sending first packet ... "));
+
+    // you can transmit C-string or Arduino string up to
+    // 256 characters long
+    // transmissionState = radio.startTransmit("Hello World!");
+
+    // you can also transmit byte array up to 256 bytes long
+    // byte byteArr[] = {0x01, 0x23, 0x45, 0x67,
+    //                   0x89, 0xAB, 0xCD, 0xEF
+    //                  };
+    // state = radio.startTransmit(byteArr, 8);
+
+
+    transmissionState = radio.startTransmit((uint8_t *)&counter, 4);
+}
+
+
+void loop()
+{
+// check if the previous transmission finished
+    if (transmittedFlag) {
+        // disable the interrupt service routine while
+        // processing the data
+        enableInterrupt = false;
+
+        // reset flag
+        transmittedFlag = false;
+
+        if (transmissionState == ERR_NONE) {
+            // packet was successfully sent
+            Serial.println(F("transmission finished!"));
+
+            // NOTE: when using interrupt-driven transmit method,
+            //       it is not possible to automatically measure
+            //       transmission data rate using getDataRate()
+#ifdef HAS_DISPLAY
+            if (u8g2) {
+                u8g2->clearBuffer();
+                u8g2->drawStr(0, 12, "Transmitting: OK!");
+                u8g2->drawStr(0, 30, ("TX:" + String(counter)).c_str());
+                u8g2->sendBuffer();
+            }
+#endif
+        } else {
+            Serial.print(F("failed, code "));
+            Serial.println(transmissionState);
+        }
+
+        // wait a second before transmitting again
+        delay(500);
+
+        // send another one
+        Serial.print(F("[SX1280] Sending another packet ... "));
+
+        // you can transmit C-string or Arduino string up to
+        // 256 characters long
+        // transmissionState = radio.startTransmit("Hello World!");
+
+        // you can also transmit byte array up to 256 bytes long
+        /*
+          byte byteArr[] = {0x01, 0x23, 0x45, 0x67,
+                            0x89, 0xAB, 0xCD, 0xEF};
+          int state = radio.startTransmit(byteArr, 8);
+        */
+
+        transmissionState = radio.startTransmit((uint8_t *)&counter, 4);
+
+        counter++;
+        // we're ready to send more packets,
+        // enable interrupt service routine
+        enableInterrupt = true;
+    }
+}
+
+
