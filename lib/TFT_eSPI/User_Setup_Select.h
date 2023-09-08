68ce26ace1a57a0bc8ff235946cd283b602837ff
lewishe <lewishe@outlook.com>
2023-09-08 10:06:48 +0800

    TFT_eSPI default select T-Beam-TFT-Shield

--- lib/TFT_eSPI/User_Setup_Select.h
+++ lib/TFT_eSPI/User_Setup_Select.h
@@ -16,7 +16,7 @@
 // unmodified or adapted for a particular hardware configuration.
 
 #ifndef USER_SETUP_LOADED //  Lets PlatformIO users define settings in
-                          //  platformio.ini, see notes in "Tools" folder.
+//  platformio.ini, see notes in "Tools" folder.
 
 ///////////////////////////////////////////////////////
 //   User configuration selection lines are below    //
@@ -24,7 +24,7 @@
 
 // Only ONE line below should be uncommented to define your setup.  Add extra lines and files as needed.
 
-#include <User_Setup.h>           // Default setup is root library folder
+// #include <User_Setup.h>           // Default setup is root library folder
 
 //#include <User_Setups/Setup1_ILI9341.h>  // Setup file for ESP8266 configured for my ILI9341
 //#include <User_Setups/Setup2_ST7735.h>   // Setup file for ESP8266 configured for my ST7735
@@ -46,7 +46,7 @@
 //#include <User_Setups/Setup17_ePaper.h>            // Setup file for ESP8266 and any Waveshare ePaper display
 //#include <User_Setups/Setup18_ST7789.h>            // Setup file for ESP8266 configured for ST7789
 
-//#include <User_Setups/Setup19_RM68140_Parallel.h>	 // Setup file configured for RM68140 with parallel bus
+//#include <User_Setups/Setup19_RM68140_Parallel.h>  // Setup file configured for RM68140 with parallel bus
 
 //#include <User_Setups/Setup20_ILI9488.h>           // Setup file for ESP8266 and ILI9488 SPI bus TFT
 //#include <User_Setups/Setup21_ILI9488.h>           // Setup file for ESP32 and ILI9488 SPI bus TFT
@@ -123,11 +123,11 @@
 
 //#include <User_Setups/Setup202_SSD1351_128.h>      // Setup file for ESP32/ESP8266 based SSD1351 128x128 1.5inch OLED display
 
-//#include <User_Setups/Setup203_ST7789.h>     // Setup file for ESP32/ESP8266 based ST7789 240X280 1.69inch TFT 
+//#include <User_Setups/Setup203_ST7789.h>     // Setup file for ESP32/ESP8266 based ST7789 240X280 1.69inch TFT
 
-//#include <User_Setups/Setup204_ESP32_TouchDown.h>     // Setup file for the ESP32 TouchDown based on ILI9488 480 x 320 TFT 
+//#include <User_Setups/Setup204_ESP32_TouchDown.h>     // Setup file for the ESP32 TouchDown based on ILI9488 480 x 320 TFT
 
-//#include <User_Setups/Setup205_ESP32_TouchDown_S3.h>     // Setup file for the ESP32 TouchDown S3 based on ILI9488 480 x 320 TFT 
+//#include <User_Setups/Setup205_ESP32_TouchDown_S3.h>     // Setup file for the ESP32 TouchDown S3 based on ILI9488 480 x 320 TFT
 
 //#include <User_Setups/Setup206_LilyGo_T_Display_S3.h>     // For the LilyGo T-Display S3 based ESP32S3 with ST7789 170 x 320 TFT
 //#include <User_Setups/Setup207_LilyGo_T_HMI.h>            // For the LilyGo T-HMI S3 based ESP32S3 with ST7789 240 x 320 TFT
@@ -135,7 +135,7 @@
 //#include <User_Setups/Setup210_LilyGo_T_Embed_S3.h>         // For the LilyGo T-Embed S3 based ESP32S3 with ST7789 170 x 320 TFT
 //#include <User_Setups/Setup211_LilyGo_T_QT_Pro_S3.h>         // For the LilyGo T-QT Pro S3 based ESP32S3 with GC9A01 128 x 128 TFT
 // #include <User_Setups/Setup212_LilyGo_T_PicoPro.h>         // For the LilyGo T-PICO-Pro with ST7796 222 x 480 TFT
-// #include <User_Setups/Setup213_LilyGo_T_Beam_Shield.h>         // For the LilyGo T-BEAM V1.x with ST7796 222 x 480 TFT
+#include <User_Setups/Setup213_LilyGo_T_Beam_Shield.h>         // For the LilyGo T-BEAM V1.x with ST7796 222 x 480 TFT
 
 //#include <User_Setups/Setup250_ESP32_S3_Box_Lite.h>      // For the ESP32 S3 Box Lite
 //#include <User_Setups/Setup251_ESP32_S3_Box.h>            // For the ESP32 S3 Box
@@ -173,110 +173,110 @@
 
 // Legacy setup support, RPI_DISPLAY_TYPE replaces RPI_DRIVER
 #if defined (RPI_DRIVER)
-  #if !defined (RPI_DISPLAY_TYPE)
-    #define RPI_DISPLAY_TYPE
-  #endif
+#if !defined (RPI_DISPLAY_TYPE)
+#define RPI_DISPLAY_TYPE
+#endif
 #endif
 
 // Legacy setup support, RPI_ILI9486_DRIVER form is deprecated
-// Instead define RPI_DISPLAY_TYPE and also define driver (e.g. ILI9486_DRIVER) 
+// Instead define RPI_DISPLAY_TYPE and also define driver (e.g. ILI9486_DRIVER)
 #if defined (RPI_ILI9486_DRIVER)
-  #if !defined (ILI9486_DRIVER)
-    #define ILI9486_DRIVER
-  #endif
-  #if !defined (RPI_DISPLAY_TYPE)
-    #define RPI_DISPLAY_TYPE
-  #endif
+#if !defined (ILI9486_DRIVER)
+#define ILI9486_DRIVER
+#endif
+#if !defined (RPI_DISPLAY_TYPE)
+#define RPI_DISPLAY_TYPE
+#endif
 #endif
 
 // Invoke 18 bit colour for selected displays
 #if !defined (RPI_DISPLAY_TYPE) && !defined (TFT_PARALLEL_8_BIT) && !defined (TFT_PARALLEL_16_BIT) && !defined (ESP32_PARALLEL)
-  #if defined (ILI9481_DRIVER) || defined (ILI9486_DRIVER) || defined (ILI9488_DRIVER)
-    #define SPI_18BIT_DRIVER
-  #endif
+#if defined (ILI9481_DRIVER) || defined (ILI9486_DRIVER) || defined (ILI9488_DRIVER)
+#define SPI_18BIT_DRIVER
+#endif
 #endif
 
 // Load the right driver definition - do not tinker here !
 #if   defined (ILI9341_DRIVER) || defined(ILI9341_2_DRIVER) || defined (ILI9342_DRIVER)
-     #include <TFT_Drivers/ILI9341_Defines.h>
-     #define  TFT_DRIVER 0x9341
+#include <TFT_Drivers/ILI9341_Defines.h>
+#define  TFT_DRIVER 0x9341
 #elif defined (ST7735_DRIVER)
-     #include <TFT_Drivers/ST7735_Defines.h>
-     #define  TFT_DRIVER 0x7735
+#include <TFT_Drivers/ST7735_Defines.h>
+#define  TFT_DRIVER 0x7735
 #elif defined (ILI9163_DRIVER)
-     #include <TFT_Drivers/ILI9163_Defines.h>
-     #define  TFT_DRIVER 0x9163
+#include <TFT_Drivers/ILI9163_Defines.h>
+#define  TFT_DRIVER 0x9163
 #elif defined (S6D02A1_DRIVER)
-     #include <TFT_Drivers/S6D02A1_Defines.h>
-     #define  TFT_DRIVER 0x6D02
+#include <TFT_Drivers/S6D02A1_Defines.h>
+#define  TFT_DRIVER 0x6D02
 #elif defined (ST7796_DRIVER)
-      #include "TFT_Drivers/ST7796_Defines.h"
-      #define  TFT_DRIVER 0x7796
+#include "TFT_Drivers/ST7796_Defines.h"
+#define  TFT_DRIVER 0x7796
 #elif defined (ILI9486_DRIVER)
-     #include <TFT_Drivers/ILI9486_Defines.h>
-     #define  TFT_DRIVER 0x9486
+#include <TFT_Drivers/ILI9486_Defines.h>
+#define  TFT_DRIVER 0x9486
 #elif defined (ILI9481_DRIVER)
-     #include <TFT_Drivers/ILI9481_Defines.h>
-     #define  TFT_DRIVER 0x9481
+#include <TFT_Drivers/ILI9481_Defines.h>
+#define  TFT_DRIVER 0x9481
 #elif defined (ILI9488_DRIVER)
-     #include <TFT_Drivers/ILI9488_Defines.h>
-     #define  TFT_DRIVER 0x9488
+#include <TFT_Drivers/ILI9488_Defines.h>
+#define  TFT_DRIVER 0x9488
 #elif defined (HX8357D_DRIVER)
-     #include "TFT_Drivers/HX8357D_Defines.h"
-     #define  TFT_DRIVER 0x8357
+#include "TFT_Drivers/HX8357D_Defines.h"
+#define  TFT_DRIVER 0x8357
 #elif defined (EPD_DRIVER)
-     #include "TFT_Drivers/EPD_Defines.h"
-     #define  TFT_DRIVER 0xE9D
+#include "TFT_Drivers/EPD_Defines.h"
+#define  TFT_DRIVER 0xE9D
 #elif defined (ST7789_DRIVER)
-     #include "TFT_Drivers/ST7789_Defines.h"
-     #define  TFT_DRIVER 0x7789
+#include "TFT_Drivers/ST7789_Defines.h"
+#define  TFT_DRIVER 0x7789
 #elif defined (R61581_DRIVER)
-     #include "TFT_Drivers/R61581_Defines.h"
-     #define  TFT_DRIVER 0x6158
+#include "TFT_Drivers/R61581_Defines.h"
+#define  TFT_DRIVER 0x6158
 #elif defined (ST7789_2_DRIVER)
-     #include "TFT_Drivers/ST7789_2_Defines.h"
-     #define  TFT_DRIVER 0x778B
+#include "TFT_Drivers/ST7789_2_Defines.h"
+#define  TFT_DRIVER 0x778B
 #elif defined (RM68140_DRIVER)
-     #include "TFT_Drivers/RM68140_Defines.h"
-     #define  TFT_DRIVER 0x6814
+#include "TFT_Drivers/RM68140_Defines.h"
+#define  TFT_DRIVER 0x6814
 #elif defined (SSD1351_DRIVER)
-     #include "TFT_Drivers/SSD1351_Defines.h"
-     #define  TFT_DRIVER 0x1351
+#include "TFT_Drivers/SSD1351_Defines.h"
+#define  TFT_DRIVER 0x1351
 #elif defined (SSD1963_480_DRIVER)
-     #include "TFT_Drivers/SSD1963_Defines.h"
-     #define  TFT_DRIVER 0x1963
+#include "TFT_Drivers/SSD1963_Defines.h"
+#define  TFT_DRIVER 0x1963
 #elif defined (SSD1963_800_DRIVER)
-     #include "TFT_Drivers/SSD1963_Defines.h"
-     #define  TFT_DRIVER 0x1963
+#include "TFT_Drivers/SSD1963_Defines.h"
+#define  TFT_DRIVER 0x1963
 #elif defined (SSD1963_800ALT_DRIVER)
-     #include "TFT_Drivers/SSD1963_Defines.h"
-     #define  TFT_DRIVER 0x1963
+#include "TFT_Drivers/SSD1963_Defines.h"
+#define  TFT_DRIVER 0x1963
 #elif defined (SSD1963_800BD_DRIVER)
-     #include "TFT_Drivers/SSD1963_Defines.h"
-     #define  TFT_DRIVER 0x1963
+#include "TFT_Drivers/SSD1963_Defines.h"
+#define  TFT_DRIVER 0x1963
 #elif defined (GC9A01_DRIVER)
-     #include "TFT_Drivers/GC9A01_Defines.h"
-     #define  TFT_DRIVER 0x9A01
+#include "TFT_Drivers/GC9A01_Defines.h"
+#define  TFT_DRIVER 0x9A01
 #elif defined (ILI9225_DRIVER)
-     #include "TFT_Drivers/ILI9225_Defines.h"
-     #define  TFT_DRIVER 0x9225
+#include "TFT_Drivers/ILI9225_Defines.h"
+#define  TFT_DRIVER 0x9225
 #elif defined (RM68120_DRIVER)
-     #include "TFT_Drivers/RM68120_Defines.h"
-     #define  TFT_DRIVER 0x6812
+#include "TFT_Drivers/RM68120_Defines.h"
+#define  TFT_DRIVER 0x6812
 #elif defined (HX8357B_DRIVER)
-     #include "TFT_Drivers/HX8357B_Defines.h"
-     #define  TFT_DRIVER 0x835B
+#include "TFT_Drivers/HX8357B_Defines.h"
+#define  TFT_DRIVER 0x835B
 #elif defined (HX8357C_DRIVER)
-     #include "TFT_Drivers/HX8357C_Defines.h"
-     #define  TFT_DRIVER 0x835C
+#include "TFT_Drivers/HX8357C_Defines.h"
+#define  TFT_DRIVER 0x835C
 
-                              // <<<<<<<<<<<<<<<<<<<<<<<< ADD NEW DRIVER HERE
-                              // XYZZY_init.h and XYZZY_rotation.h must also be added in TFT_eSPI.cpp
+// <<<<<<<<<<<<<<<<<<<<<<<< ADD NEW DRIVER HERE
+// XYZZY_init.h and XYZZY_rotation.h must also be added in TFT_eSPI.cpp
 #elif defined (XYZZY_DRIVER)
-     #include "TFT_Drivers/XYZZY_Defines.h"
-     #define  TFT_DRIVER 0x0000
+#include "TFT_Drivers/XYZZY_Defines.h"
+#define  TFT_DRIVER 0x0000
 #else
-     #define  TFT_DRIVER 0x0000
+#define  TFT_DRIVER 0x0000
 #endif
 
 // These are the pins for ESP8266 boards
